a48ba7e520ae89941b2cc0dd796e5219707023e8
HanPhan <han@drobebox.com>
2021-04-27 17:34:34 +0700

    disable node_modules cache

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import { render } from "react-dom";
 import App from "./App";
 import "./style.css";
 
+const appHeight = () => {
+  const doc = document.documentElement;
+  doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+};
+window.addEventListener("resize", appHeight);
+appHeight();
+
 render(<App />, document.getElementById("app"));
